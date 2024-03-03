de518cc996a81a18c57c3aca9aab5fe9b58319d4
Darsh Patel <darshpatel@ebsco.com>
2024-03-03 13:05:28 +0530

    fixing match type

--- src/components/match-detail/match-detail.tsx
+++ src/components/match-detail/match-detail.tsx
@@ -8,15 +8,14 @@ interface MatchProps {
 }
 
 const MatchDetail: React.FC<MatchProps> = ({ matches }) => {
-
     return(
         <>
         <h1>Matches</h1>
-        {matches.map(({id, match_date, show_name, wrestlers_involved, winner_id, match_type}) => 
+        {matches.map(({id, match_date, show_name, wrestlers_involved_name, winner_id, match_type}) => 
                 (<div className={styles.gridContainer} key={id}>
                     <div className={styles.date}>{formatDate(match_date)}</div>
                     <div className={styles.showName}>{show_name}</div>
-                    <div className={styles.wrestlersInvolved}>{formatWrestlerNames(wrestlers_involved)}</div>
+                    <div className={styles.wrestlersInvolved}>{formatWrestlerNames(wrestlers_involved_name)}</div>
                     <div className={styles.winnerId}>{winner_id}</div>
                     <div className={styles.matchType}>{match_type}</div>
                 </div>))}
