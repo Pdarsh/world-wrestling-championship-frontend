238c85b76da95fa459c28866a3b80768de289932
Darsh Patel <darshpatel@ebsco.com>
2024-03-03 14:05:36 +0530

    adding next-redux-wrapper and making change accordingly

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,17 +4,8 @@ import Layout from "@/components/layout/layout";
 import Navbar from "@/components/navbar/navbar";
 import styles from "@/styles/index.module.scss";
 import TransparentButton from "@/components/transparent-button/transparent-button";
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "@/redux/store";
-import { fetchWrestlers } from "@/redux/actions";
 
 const Home: React.FC = () => {
-  const dispatch: AppDispatch = useDispatch();
-
-  useEffect(() => {
-    // Fetch wrestlers' data when the component mounts
-    dispatch(fetchWrestlers());
-  }, [dispatch]);
 
   return (
     <Layout>
